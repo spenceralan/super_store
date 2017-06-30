da8d2e3cf4c83f086a0b79f6d91d6ceace492d27
Spencer Alan <spencer.alan@icloud.com>
2017-06-30 11:24:15 -0700

    update models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+  has_many :reviews
+
+  validates :name, :presence => true
+  validates :price, :presence => true
+  validates :country, :presence => true
+
+  scope :most_reviewed, -> {(
+    select("products.*, count(review.id) as reviews_count")
+    .joins(:comments)
+    .group("products.id")
+    .order("comments_count DESC")
+    .limit(1)
+  )}
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,20 @@
 class Review < ApplicationRecord
+  belongs_to :product
+
+  validates :username, presence: true
+  
+  validates :content,
+    presence: true,
+    length: {
+      in: 50..250,
+      message: "The review's content must be between 50 and 250 characters."
+    }
+
+  validates :rating,
+    presence: true,
+    inclusion: { 
+      in: 1..5,
+      message: "Please select a number between 1 and 5"
+    }
+
 end
