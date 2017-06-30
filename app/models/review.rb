1838979e7c4f8d97fecff94c2f59ad60fa81bd1f
Spencer Alan <spencer.alan@icloud.com>
2017-06-30 14:12:24 -0700

    update tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,15 +6,13 @@ class Review < ApplicationRecord
   validates :content,
     presence: true,
     length: {
-      in: 50..250,
-      message: "The review's content must be between 50 and 250 characters."
+      in: 50..250
     }
 
   validates :rating,
     presence: true,
     inclusion: { 
-      in: 1..5,
-      message: "Please select a number between 1 and 5"
+      in: 1..5
     }
 
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,5 +5,10 @@ RSpec.describe Review, type: :model do
   it { should validate_presence_of :content }
   it { should validate_presence_of :rating }
 
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
+  
+  it { should validate_length_of(:content).is_at_least(50).is_at_most(250) }
+
+
   it { should belong_to :product }
 end
