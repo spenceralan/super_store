e7ab346e5478ea4ab28ed867425b6628741e0333
Spencer Alan <spencer.alan@icloud.com>
2017-06-30 14:53:22 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Product.destroy_all
+
+50.times do
+  product = Product.create!(
+    name: Faker::Book.title,
+    price: Faker::Number.decimal(2),
+    country: Faker::Address.country,
+  )
+  Faker::Number.between(3, 30).times do
+    product.reviews.create!(
+      username: Faker::RickAndMorty.character,
+      content: Faker::Hipster.sentence(10, true, 5),
+      rating: Faker::Number.between(1, 5),
+    )
+  end
+end
